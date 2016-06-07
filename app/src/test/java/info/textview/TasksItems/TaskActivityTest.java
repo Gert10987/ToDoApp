e1edfaec85f14c34f3b2b7b193cd503bc0947279
Gert <pawel.kalasz@gmail.com>
2016-06-07 21:37:22 +0200

    testing with mockito

--- app/src/test/java/info/textview/TasksItems/TaskActivityTest.java
+++ app/src/test/java/info/textview/TasksItems/TaskActivityTest.java
@@ -2,27 +2,21 @@ package info.textview.TasksItems;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.stubbing.Answer;
 
-import static org.mockito.Mockito.verify;
+import org.mockito.MockitoAnnotations;
+
+import info.textview.DataBase.DataBaseToDo;
+
+
 import static org.mockito.Mockito.when;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Matchers.contains;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by Paweł on 2016-06-03.
  */
 public class TaskActivityTest {
-
-
-    @Mock
-    TasksActivity tasksActivity;
 
 
     @Before
@@ -35,13 +29,32 @@ public class TaskActivityTest {
     }
 
     @Test
-    public void testGetSecondsFormDuration(){
+    public void testGetTimeInIntegerFromString() {
 
-        tasksActivity = new TasksActivity();
-        when(tasksActivity.getSecondsFromDurationString("00:00")).thenReturn(0);
+        String testTime = "33:00:45";
+        int testHours = 33;
+        int testMinutes = 00;
+        int testSeconds = 45;
 
+        TasksActivity tasksActivity = mock(TasksActivity.class);
 
+        when(tasksActivity.getSecondsFromDurationString(testTime)).
+                thenReturn(testSeconds + (testMinutes * 60) + (testHours * 3600));
+
+        assertEquals(tasksActivity.getSecondsFromDurationString(testTime),
+                testSeconds + (testMinutes * 60) + (testHours * 3600));
 
     }
+
+    @Test(expected=NullPointerException.class)
+    public void testForNullPointerException() {
+        // create an configure mock
+        DataBaseToDo dataBaseToDo = mock(DataBaseToDo.class);
+        doThrow(new NullPointerException()).when(dataBaseToDo).setNameOfTable(null);
+
+        // use mock
+        dataBaseToDo.setNameOfTable(null);
+    }
+
 
 }
